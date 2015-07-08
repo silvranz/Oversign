6a1e97ab47ea9cfa0c687025fac9578da2caf344
silvranz <silvranz@gmail.com>
2015-07-08 15:13:12 +0700

    update global function

--- index.php
+++ index.php
@@ -55,7 +55,12 @@
 					}
 				})
 			}
-			
+			function defaultValue(variable,value){
+				return typeof variable !== "undefined"?  variable:value;
+			}
+			function defaultPopupAnimation(){
+				return {};
+			}
 			$(document).ready(function(){
 				$("*").click(function(e){
 					if($(this).hasClass("blog-nav-item")){
@@ -64,6 +69,25 @@
 					}					
 				});
 				loadContent("home");
+				$.fn.customPopup = function(mode,animation,time){
+					mode = defaultValue(mode,"fullscreen");
+					animation = defaultValue(animation,defaultPopupAnimation());
+					time = defaultValue(time,500);
+					switch(mode){
+						case "fullscreen":
+							
+							break;
+						case "left":
+							break;
+						case "right":
+							break;
+						case "top":
+							break;
+						case "bottom":
+							break;
+					}
+					return this;
+				}
 			});
 		</script>
 	</head>
