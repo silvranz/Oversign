6d3607d2b8c47f101a3b085bc52cf3989356d25d
Angela Muliawan <amuliawan93@gmail.com>
2015-07-16 16:37:14 +0700

    ubah main.js buat click register n forgot password

--- source/library/js/main.js
+++ source/library/js/main.js
@@ -48,6 +48,31 @@ function fullScreenPopup(content){
 	$("body").append(blocker);
 }
 $(document).ready(function(){
+	$("#btnRegisterNewMember").click(function(){
+		$(".loginForm").fadeOut(function(){
+			$(".registerForm").fadeIn();
+		});
+	})
+	$("#btnBackToLoginReg").click(function(){
+		$(".registerForm").fadeOut(function(){
+			$(".loginForm").fadeIn();
+		});
+	})
+	$("#btnForgotPassword").click(function(){
+		$(".loginForm").fadeOut(function(){
+			$(".forgotPasswordForm").fadeIn();
+			$("#modalLabel").text("Forgot Password");
+			$("#modalLabelContent").text("We will send confirmation and new password to your email.");
+		});
+	})
+	$("#btnBackToLoginFor").click(function(){
+		$(".forgotPasswordForm").fadeOut(function(){
+			$(".loginForm").fadeIn();
+			$("#modalLabel").text("Login | Register");
+			$("#modalLabelContent").text("Input email and password to start managing your website.");
+		});
+	})
+
 	var onloadTarget = window.location.href.replace(BASE_URL,"");
 	onloadTarget = onloadTarget==""?"home":onloadTarget;
 	$("*").click(function(e){
