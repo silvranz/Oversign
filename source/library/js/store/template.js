398e7d2a5762a38eecc93487087bd969eb9939e6
silvranz <silvranz@gmail.com>
2015-08-07 04:47:46 +0700

    store template

---
+++ source/library/js/store/template.js
@@ -0,0 +1,38 @@
+var listTemplate = [];
+function loadTemplate(){
+	var parentElement = $("#displayTemplate");
+	$(listTemplate).each(function(i){
+		var newItem = $("#templateItem",parentElement).clone().removeAttr("id").show();
+		$(".templateName",newItem).text(this.templateName);
+		$(".previewImage",newItem).attr("alt",this.previewImage);
+		$(".rating",newItem).text(this.rating+" star ");
+		$(".numUser",newItem).text(this.userRating);
+		$(parentElement).append(newItem);
+	})
+}
+$(document).ready(function(){
+	$.ajax({
+		url:"http://localhost:8080/oversign/service/store/getAllTemplateCategory",
+		type:"GET",
+		crossDomain:true,
+		success:function(data){
+			$(data).each(function(i){
+				var categoryItem = $("<a href='#'>");
+				$(categoryItem).text(this.categoryName).click(function(e){
+					e.preventDefault();
+				});
+				console.log(categoryItem);
+				$("#categoryFilter").append($("<li>").append(categoryItem));
+			})		
+		}
+	})
+	$.ajax({
+		url:"http://localhost:8080/oversign/service/store/getAllTemplate",
+		type:"GET",
+		crossDomain:true,
+		success:function(data){
+			listTemplate = data;
+			loadTemplate();
+		}
+	})
+});
