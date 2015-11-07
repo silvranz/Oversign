dcb02652a6e13dbdcf5db984aa0c9645427dce88
Angela <amuliawan93@gmail.com>
2015-11-07 09:01:15 +0700

    help

--- source/library/js/forum.js
+++ source/library/js/forum.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+	alert('aa');
+	$('.help .category ul li').click(function(e){
+		e.preventDefault();
+		var self = $(this);
+		var content_selector = $('#'+self.attr('data-id'));
+
+		$('.help .content').stop().fadeOut(200);
+		$('.help .content').promise().done(function(){
+			$('.help .content ul li div.description').hide();
+			content_selector.stop().fadeIn(200);
+		});
+	});
+
+	$('.help span.title').click(function(e){
+		e.preventDefault();
+		var self = $(this);
+		self.closest('li').find('div.description').stop().slideToggle();
+	});
+
+/*
 	$.ajax({
 		url:"http://localhost:8080/oversign/service/forum/getpopularhashtag",
 		type:"GET",
@@ -38,4 +58,5 @@ $(document).ready(function(){
 			}
 		})
 	//})
+*/
 });
